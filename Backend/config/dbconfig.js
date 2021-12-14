c6302b535947cf8d6b2448d2fa63ed5ae38460e6
Prathamesh <2002pratham1109@gmail.com>
2021-12-14 14:31:02 +0530

    db-login details deleted

--- Backend/config/dbconfig.js
+++ Backend/config/dbconfig.js
@@ -1,8 +1,6 @@
 const db = {
     HOST : "localhost",
-    // USER : "id18019295_dbmsuser",
     USER : "root",
-    // PASSWORD : "Pratham@12345",
     PASSWORD : "",
     DATABASE : "id18019295_dbms"
 };
